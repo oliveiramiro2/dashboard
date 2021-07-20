d03bd4184d809a1a2a876fadb4891f3abf4a9e89
oliveiramiro2 <ramiromares95@gmail.com>
2021-07-19 23:03:59 -0300

    função de exibição de informações add aos icones de exemplo

--- app/src/componentes/NavbarAcoes.js
+++ app/src/componentes/NavbarAcoes.js
@@ -1,23 +1,45 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { FaFacebook } from 'react-icons/fa'
 import { SiFord, SiVisa, SiMcdonalds } from 'react-icons/si'
 import { AiOutlineApple } from 'react-icons/ai'
 
-function NavbarAcoes(){
+import { changeCompany } from './../store/actions/TrocaEmpresa'
+
+function NavbarAcoes({ changeCompany }){
+    const showExample = empresa => {
+        changeCompany(
+            `https://cloud.iexapis.com/v1/stock/${empresa}/quote/?token=pk_c778b6dbd2154d6fa15043568d469931`
+        )
+    }
 
     return(
         <nav>
             <h5 className='info-exemplo'>Empresas Conhecidas</h5> 
             <div className='div-exemplo'>
-                <p className='icone-exemplo face'><FaFacebook /></p>
-                <p className='icone-exemplo ford'><SiFord /></p>
-                <p className='icone-exemplo mcdonald'><SiMcdonalds /></p>
-                <p className='icone-exemplo visa'><SiVisa /></p>
-                <p className='icone-exemplo apple'><AiOutlineApple /></p> 
+                <p className='icone-exemplo face' onClick={()=>showExample('fb')}>
+                    <FaFacebook />
+                </p>
+                <p className='icone-exemplo ford' onClick={()=>showExample('f')}>
+                    <SiFord />
+                </p>
+                <p className='icone-exemplo mcdonald' onClick={()=>showExample('mcd')}>
+                    <SiMcdonalds />
+                </p>
+                <p className='icone-exemplo visa' onClick={()=>showExample('v')}>
+                    <SiVisa />
+                </p>
+                <p className='icone-exemplo apple' onClick={()=>showExample('aapl')}>
+                    <AiOutlineApple />
+                </p> 
             </div>
             
         </nav>
     )
 }
 
-export default NavbarAcoes
+const mapStateToProps = state => {
+    return { informacoes: state.informacoes }
+}
+
+export default connect(mapStateToProps, { changeCompany })(NavbarAcoes)
